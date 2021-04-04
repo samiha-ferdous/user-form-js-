6a79bb3381738c11262e50aedd30babf4559b89f
[samiha ferdous] <[samiha.ferdous@northsouth.edu]>
2021-04-04 19:33:49 +0600

    internal changes

--- page.js
+++ page.js
@@ -3,3 +3,33 @@ const username=document.getElementById("username");
 const email=document.getElementById("email");
 const password=document.getElementById("password");
 const confirmpassword=document.getElementById("confirmpassword");
+
+//show error function
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = input.nextElementSibling; // update here 
+    small.innerText = message;
+  }
+  
+
+//success function
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success"; 
+}
+
+function CheckErrors(inputArr){
+    inputArr.forEach(function(input){
+    if(input.value.trim()===''){
+        showError(input,`${input.id} is required`);
+}
+    });
+
+}
+
+form.addEventListener('submit',function(x){
+    x.preventDefault();
+    CheckErrors([username,email,password,confirmpassword]);
+});
